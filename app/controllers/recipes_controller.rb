d6116d3e16e532aeed5ed8ddff05df4c204e808e
Riaz Mohamed <riaz.ahamed23@gmail.com>
2016-06-16 06:46:54 +0000

    create new and edit forms and partials

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,4 +7,42 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
   
+  def new
+    @recipe = Recipe.new
+  end
+  
+  def create
+    @recipe = Recipe.create(recipe_params)
+    @recipe.chef = Chef.find(2)
+    
+    if @recipe.save
+      flash[:success] = "This recipe was created successfully!!!"
+      redirect_to recipes_path
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = "This recipe was edited successfully!!!"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    
+  end
+  
+  private
+    def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description)
+    end
 end
